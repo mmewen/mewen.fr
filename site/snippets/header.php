cb3f91fb414eb1cedb1ca7463604c656158306f3
Mewen Michel <mewenmichel@yahoo.fr>
2020-01-20 13:58:51 +0100

    Add favicon

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -14,6 +14,8 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
+  <link rel="icon" type="image/png" href="favicon.png">
+
   <!-- The title tag we show the title of our site and the title of the current page -->
   <title><?= $site->title() ?> | <?= $page->title() ?></title>
 
