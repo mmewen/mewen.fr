5e81a15b654dbd23c19bf78fb8c28002513ba265
Mewen Michel <mewenmichel@yahoo.fr>
2020-01-20 10:05:30 +0100

    Remove old version

--- inc/scripts/OLD/form.js
+++
@@ -1,9 +0,0 @@
-function fenetre(x){
-        if(x){
-                document.getElementById("form").style.display="block";
-                return false;
-        }else{
-                document.getElementById("form").style.display="none";
-                return false;
-        }
-};
