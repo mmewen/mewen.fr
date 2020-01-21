8ac43db1774d820a1ec66d798ee863e49bc3b873
Mewen Michel <mewenmichel@yahoo.fr>
2020-01-21 14:49:05 +0100

    Job pages without projet list

--- site/templates/job.php
+++ site/templates/job.php
@@ -1,17 +1,33 @@
 <?php snippet('header') ?>
 
 <main>
-  <div>
-    <img src="<?= $page->cover()->url() ?>">
+  <div class="cover-container">
+    <?php if($image = $page->cover()->toFile()): ?>
+      <div class="o-blue">
+        <img src="<?= $image->url() ?>"></div>
+    <?php endif ?>
+
+    <?php if(!empty(trim($page->presentation()->kt()))): ?>
+    <div class="cover-shade"></div>
+    <div class="cover-text-container flex vertical-flex">
+      <div class="cover-text">
+        <?= $page->presentation()->kt() ?>
+      </div>
+    </div>
+    <?php endif ?>
   </div>
-  <div class="text">
-    <?= $page->presentation()->kt() ?>
-  </div>
+
   <div class="text">
     <?= $page->text()->kt() ?>
   </div>
-  <div class="text">
-    <?= $page->contact_text()->kt() ?>
+
+  <div class="contact-container">
+    <div class="contact">
+      <?= $page->contact_text()->kt() ?>
+
+      <?= html::email($site->email()) ?>
+      <?= html::tel($site->phone()) ?>
+    </div>
   </div>
 </main>
 
