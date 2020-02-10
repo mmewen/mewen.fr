54703bd9715d2f8dd58dfa979a6f403b1e519ca1
Mewen Michel <mewenmichel@yahoo.fr>
2020-02-10 20:17:05 +0100

    Quick implementation for media meta data

--- site/snippets/header.php
+++ site/snippets/header.php
@@ -18,6 +18,18 @@
 
   <!-- The title tag we show the title of our site and the title of the current page -->
   <title><?= $site->title() ?> <?= $page->title() ?></title>
+  <meta property="description" content="<?= $site->description() ?>" />
+
+  <!-- Social media meta data -->
+  <meta property="og:site_name" content="<?= $site->title() ?> - <?= $page->title() ?>" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="/assets/images/default.jpg" />
+  <meta property="og:locale:alternate" content="fr_FR" />
+  <meta property="og:fb:admins" content="105057877735885" />
+  <meta property="og:url" content="<?= $page->url() ?>" />
+  <meta property="og:description" content="<?= $site->description() ?>" />
+  <meta name="twitter:title" content="<?= $site->title() ?> - <?= $page->title() ?>" />
+  <meta name="twitter:card" content="<?= $site->description() ?>" />
 
   <!-- Stylesheets can be included using the `css()` helper. Kirby also provides the `js()` helper to include script file. 
         More Kirby helpers: https://getkirby.com/docs/reference/templates/helpers -->
