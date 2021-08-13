9f060bbabd3797fc18c8c488f61c9e6758776846
Mewen Michel <mewenmichel@yahoo.fr>
2021-08-13 16:21:36 +0200

    Adapt pitch + details

--- site/templates/job.php
+++ site/templates/job.php
@@ -18,22 +18,29 @@
   </section>
 
   <?php
+  $first = true;
   $project_lists = [[$page->project_list_1_title(), $page->project_list_1()],[$page->project_list_2_title(), $page->project_list_2()]];
   foreach ($project_lists as [$project_title, $project_list]): ?>
-  <section class="projects">
-    <h1><?= $project_title ?></h1>
-    <div class="flex align-start space-around">
-      <?php
-      $projects = $project_list->toStructure();
-      foreach ($projects as $project): ?>
-      <article class="project d6 m12 text">
-        <h2><?= $project->title() ?></h2>
-        <i><?= $project->subtitle() ?></i>
-        <p><?= $project->details()->kt() ?></p>
-      </article>
-      <?php endforeach ?>
-    </div>
-  </section>
+    <section class="projects">
+      <h1><?= $project_title ?></h1>
+      <div class="flex align-start space-around">
+        <?php
+        $projects = $project_list->toStructure();
+        foreach ($projects as $project): ?>
+        <article class="project d6 m12 text">
+          <h2><?= $project->title() ?></h2>
+          <i><?= $project->subtitle() ?></i>
+          <p><?= $project->details()->kt() ?></p>
+        </article>
+        <?php endforeach ?>
+      </div>
+    </section>
+
+    <?php if ($first):
+        $first = false; ?>
+        <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/0NLV5fBy1uk?controls=2&autoplay=0&fs=0&iv_load_policy=3&loop=0&modestbranding=1&rel=0&showinfo=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen style="margin: 0 auto; display: block; max-width: 100%;"></iframe>
+    <?php endif ?>
+
   <?php endforeach ?>
 
   <section class="text">
